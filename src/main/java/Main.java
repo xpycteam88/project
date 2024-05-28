72292ec7f2dbba2766ae175d39c4aefcdf68381b
xpycteam88 <sicuno@mail.ru>
2024-05-28 22:52:15 +0300

    Task done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int a, b, c;
+        a = 2;
+        b = 0;
 
+        try {
+            c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя");
+        }
     }
 }
